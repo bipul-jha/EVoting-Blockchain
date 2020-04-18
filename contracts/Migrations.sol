db38bba72de220ed0da1e169f6fa110b37991e40
bipul-jha <bipuljha143@gmail.com>
2020-04-18 19:41:58 +0530

    Cast Vote.

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.5.16;
 
 contract Migrations {
   address public owner;
@@ -8,11 +8,16 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  constructor() public {
+  constructor () public{
     owner = msg.sender;
   }
 
-  function setCompleted(uint completed) public restricted {
+  function setCompleted(uint completed) public{
     last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
   }
 }
